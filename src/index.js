200d24e455c72096842eb0eaa00c5ac09324ca75
Priya Thakur <74084538+priyathakur1234@users.noreply.github.com>
2023-06-10 20:11:31 +0545

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import reportWebVitals from './reportWebVitals';
+import 'bootstrap/dist/css/bootstrap.css';
+
+//import Counter from './components/counter';
+ReactDOM.render(<App/>,document.getElementById("root"));
+//ReactDOM.render(<Counters/>,document.getElementById("root"));
+//ReactDOM.render(<Counter/>,document.getElementById("root"));
+reportWebVitals();
